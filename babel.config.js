43fb379d230c7ee927f8cc365c84ff8435c42bdb
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-05-01 03:40:13 +0900

    chore(babel): remove unnecessary hack

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
-
-  const orig = api.caller;
-  api.caller = () => true;
-  const expo = require('babel-preset-expo')(api);
-  api.caller = orig;
-
-  expo.overrides[0].plugins
-    .find(p => p[0] === 'babel-plugin-react-native-web')
-    .push({ commonjs: true });
-  expo.presets.find(p => p[0] === '@babel/preset-env')[1] = {
-    modules: false,
-    targets: {
-      node: 'current',
-    },
-  };
-
   return {
-    presets: [expo],
+    presets: ['babel-preset-expo'],
   };
 };
