03802cdd8d9253710ca5075c33fb4d13d06876e8
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-05-01 04:00:23 +0900

    feat: don't load unused fonts

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -46,9 +46,6 @@ async function loadResourcesAsync() {
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       // This is the font that we are using for our tab bar
       ...Icon.Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free
-      // to remove this if you are not using it in your app
-      'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
     }),
   ]);
 }
